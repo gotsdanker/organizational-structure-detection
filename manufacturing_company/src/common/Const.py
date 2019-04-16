43b35842da168dfa533e2906db8eeba110e6b7d9
Mateusz Nurek <nurek.mateusz@gmail.com>
2019-04-16 23:00:01 +0200

    Fixed parsing of dates in EventDate column

---
+++ manufacturing_company/src/common/Const.py
@@ -0,0 +1,6 @@
+SENDER = 'Sender'
+RECIPIENT = 'Recipient'
+EVENT_DATE = 'EventDate'
+
+ID = 'ID'
+REPORTS_TO_ID = 'ReportsToID'
--- manufacturing_company/src/data/import_data.py
+++ manufacturing_company/src/data/import_data.py
@@ -1,8 +1,9 @@
+from manufacturing_company.src.common.Const import *
 import pandas as pd
 
 
 def load_communication():
-    return pd.read_csv('manufacturing_company/data/raw/communication.csv', sep=';', parse_dates=True)
+    return pd.read_csv('manufacturing_company/data/raw/communication.csv', sep=';', parse_dates=[EVENT_DATE])
 
 
 def load_reportsto():
